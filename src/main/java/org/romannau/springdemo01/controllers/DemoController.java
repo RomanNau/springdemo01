b7cc125d7daa9f035c134a16e06111a24e7c8d38
RomanNaun <romoolay@gmail.com>
2022-05-11 19:43:38 +0300

    added parsing after a certain time

--- src/main/java/org/romannau/springdemo01/controllers/DemoController.java
+++ src/main/java/org/romannau/springdemo01/controllers/DemoController.java
@@ -1,9 +1,8 @@
 package org.romannau.springdemo01.controllers;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.romannau.springdemo01.USDRate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,38 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.io.IOException;
 
 @Controller
+@EnableScheduling
+
 public class DemoController {
+    @Autowired
+    USDRate usdrate = new USDRate();
 
-    String dollarCourse=null;
 
 
-    @GetMapping("/")
 
-    public String parsing(Model model) throws IOException {
-        Document doc = Jsoup.connect("https://cbr.ru/").get();   /*получаем html страницу для дальнешего разбора ее в Sping*/
+    @GetMapping("/currencyRate")
 
-        Elements rateUSD = doc.getElementsByClass("main-indicator_rate");  /*ищем по тэгу "main-indicator_rate"*/
-
-        for(int i = 0; i < rateUSD.size(); i++) {
-            Element rateUSD01 = rateUSD.get(i);   /* записываем результат поиска по тэгу "main-indicator_rate"  в список */
-
-            Elements rateUSD02 = rateUSD01.getElementsByClass("_dollar");   /*по вышеполученному списку ищем по тэгу "_dollar"*/
-            if (!rateUSD02.isEmpty()) {
-                System.out.println(rateUSD01.text());    /*проверка нового списка на наполненость*/
-
-                Elements rateUSD03 = rateUSD01.getElementsByClass("mono-num");   /* поиск по тэгу "mono-num"*/
-                Element rateUSD04 = rateUSD03.get(0);  /*получение первого элемента(курс на сегодня) в списке*/
-
-                dollarCourse = rateUSD04.text();  /*перевод в текстовый формат переменной dollarCourse*/
-                break;
-            } else {
-                dollarCourse = "пустота";
-            }
-            System.out.println(dollarCourse);  /*вывод на консоль dollarCourse-необязательная проверка*/
-        return dollarCourse;
-        }
-        model.addAttribute("dollarCourse", dollarCourse);
+    public String demo(Model model) throws IOException{
+        model.addAttribute("dollarCourse", usdrate.getDollarCourse());
         return "home";
-
     }
 }
